e141d0f21f641d4770727ef6d84e2e1f6920c78c
Francisco Pereira Guimarães <franciscop.guimaraes04@gmail.com>
2023-09-25 15:17:54 -0300

    return cpf in /login

--- users/serializers.py
+++ users/serializers.py
@@ -15,4 +15,4 @@ class UserSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['email', 'password', 'cpf']
